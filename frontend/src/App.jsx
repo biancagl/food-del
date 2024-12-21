e1a4e620e8176c58e401a19be1d477107b3d3bf3
biancagl <biancagonzalez2244@gmail.com>
2024-12-21 17:04:37 -0500

    minor updates/bug fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
+
 const App = () => {
   return (
     <div className='app'>
